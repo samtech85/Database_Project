5f21b4d873a199fa61eb79da70237a5adb14b753
Marcelus Joseph Bonheur <sbonheur85@gmail.com>
2020-11-22 13:16:02 -0500

    database_pro

---
+++ main.py
@@ -0,0 +1,12 @@
+# This is an online database project
+data = ["samuel", "Marcelus", "Jimy"]
+print(data)
+user = input("Enter your name Please:")
+
+if (user in data):
+    print ("Welcome", user,"!")
+elif (user not in data):
+    new = input("You are not in our database,Please Register:")
+
+print("okay")
+    
